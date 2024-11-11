edf52ca720caf682a7919fa2a639adfa15b4a82b
Prince Jain <pj122333444@gmail.com>
2024-11-11 23:20:08 +0530

    removed global type

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,15 +7,12 @@ interface MongooseConnection {
     promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as { mongoose: MongooseConnection }).mongoose;
-if (!cached) {
-    cached = (global as { mongoose: MongooseConnection }).mongoose = {
-        conn: null,
-        promise: null
-    };
-}
+let cached: MongooseConnection = {
+    conn: null,
+    promise: null
+};
 
-export const connectToDatabase = async () => {
+export const getMongooseInstance = async () => {
     if (cached.conn) return cached.conn;
 
     if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
@@ -23,10 +20,10 @@ export const connectToDatabase = async () => {
     cached.promise =
         cached.promise ||
         mongoose.connect(MONGODB_URL, {
-            dbName: 'imaginify', bufferCommands: false
+            dbName: 'imaginify',
+            bufferCommands: false
         });
 
     cached.conn = await cached.promise;
-
     return cached.conn;
 };
