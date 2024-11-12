c5567ca278b6be7b782998e553701e1cc362518a
Prince Jain <pj122333444@gmail.com>
2024-11-12 12:31:58 +0530

    changes

--- app/api/webhooks/clerk/route.js
+++ app/api/webhooks/clerk/route.js
@@ -46,11 +46,10 @@ export async function POST(req) {
     const eventType = evt.type;
     // CREATE
     if (eventType === "user.created") {
-        const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+        const { id, email_addresses, image_url, first_name, last_name } = evt.data;
         const user = {
             clerkId: id,
             email: email_addresses[0].email_address,
-            username: username,
             firstName: first_name,
             lastName: last_name,
             photo: image_url,
@@ -68,11 +67,11 @@ export async function POST(req) {
     }
     // UPDATE
     if (eventType === "user.updated") {
-        const { id, image_url, first_name, last_name, username } = evt.data;
+        const { id, image_url, first_name, last_name } = evt.data;
         const user = {
             firstName: first_name,
             lastName: last_name,
-            username: username,
+
             photo: image_url,
         };
         const updatedUser = await updateUser(id, user);
