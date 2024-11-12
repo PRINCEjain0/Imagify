636e9c4b00386c8b4101bb6bb22c8199083f48e6
Prince Jain <pj122333444@gmail.com>
2024-11-12 12:42:48 +0530

    deleted username

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -11,11 +11,7 @@ const UserSchema = new Schema({
         required: true,
         unique: true,
     },
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
+
     photo: {
         type: String,
         required: true,
