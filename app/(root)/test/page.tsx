fccdf163825e9a14aaaf588b6880e324969794ed
Prince Jain <pj122333444@gmail.com>
2024-11-14 13:16:18 +0530

    c

--- app/(root)/test/page.tsx
+++ app/(root)/test/page.tsx
@@ -4,13 +4,7 @@ import { deleteUser } from "@/lib/actions/user.actions";
 
 export default function CreateUserButton() {
     const handleCreateUser = async () => {
-        const dummyUserData = {
 
-            photo: "https://example.com/photo.jpgddwd",
-            firstName: "Dummy",
-            lastName: "User",
-
-        };
 
         try {
             const response = await deleteUser("dummy_clerk_id_123x");
